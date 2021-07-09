46d01567de4dded24d56d3f30434e970c5e2521c
salobeko86 <85491228+salobeko86@users.noreply.github.com>
2021-07-09 13:55:41 +0100

    add script.js,

---
+++ script.js
@@ -0,0 +1,31 @@
+let name = "salome";
+let surname ="bekurashvili";
+
+//#3
+let r = 10;
+let area = r * r;
+let l = 2 * r;
+ console.log( r * r);
+ console.log( area);
+ console.log(l);
+
+ //#4
+ let a = 5;
+ let b = 10;
+ let c = (a * a + b * b);
+
+ console.log("Hypotenuse =" + c);
+
+
+//#5
+let x = prompt;
+
+console.log ("The volume of the cube =" + (x, 3));
+
+
+
+
+
+
+
+
